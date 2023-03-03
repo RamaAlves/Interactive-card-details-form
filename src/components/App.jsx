df0b89ec158b80cec827883ce83e37e511f92355
RamaAlves <ramiroalvesprof@outlook.com>
2023-03-03 10:18:58 -0300

    refactor y correccion de errores

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,10 +13,10 @@ function App() {
   //variables
   let form;
 
-  let [errorName, setErrorName]= useState([false]);
-  let [errorCardNumber, setErrorCardNumber] = useState([false]);
-  let [errorDate, setErrorDate] = useState([false]);
-  let [errorCvc, setErrorCvc]= useState([false]);
+  let [errorName, setErrorName]= useState(false);
+  let [errorCardNumber, setErrorCardNumber] = useState(false);
+  let [errorDate, setErrorDate] = useState(false);
+  let [errorCvc, setErrorCvc]= useState(false);
 
   //states
   const [cardName, setCardName] = useState(['JANE APPLESEED']);
@@ -25,7 +25,7 @@ function App() {
   const [yearExp, setYearExp] = useState(['25']);
   const [CVC, setCVC] = useState(['123']);
   const [card, setCard]= useState([])
-  const [cardAdded, setCardAdded]= useState([false])
+  const [cardAdded, setCardAdded]= useState(false)
 
   //refs
   const cardNameRef = useRef();
@@ -50,14 +50,10 @@ function App() {
   function validarName(name){
   
     if (name.length>1) {
-      setErrorName(()=>{
-        return [false]
-      })
+      setErrorName(false)
       return false
     } else{
-      setErrorName(()=>{
-        return [true]
-      })
+      setErrorName(true)
       return true
     }
   }
@@ -77,15 +73,11 @@ function App() {
 
   function validarCardNumber(number){
     let onlyNumbers = cardRegex.test(number); // Devuelve true si todos los caracteres son numeros
-    if (onlyNumbers && number!==''&& number[0].length>=16){
-      setErrorCardNumber(()=>{
-        return [false]
-      })
+    if (onlyNumbers && number!==''&& number.length>=16){
+      setErrorCardNumber(false)
       return false
     }else{
-      setErrorCardNumber(()=>{
-        return[true]
-      })
+      setErrorCardNumber(true)
       return true
     }
   }
@@ -129,14 +121,10 @@ function App() {
   function validarDate(date){
     let onlyNumbers = regex.test(date) //devuelve true si todos los caracteres son numeros
     if (onlyNumbers && date!==''){
-      setErrorDate(()=>{
-        return[false]
-      })
+      setErrorDate(false)
       return false
     }else{
-      setErrorDate(()=>{
-        return[true]
-      })
+      setErrorDate(true)
       return true
     }
     
@@ -156,16 +144,12 @@ function App() {
     let onlyNumbers = regex.test(cvc)
     
     if (onlyNumbers&&cvc!==''){
-      setErrorCvc(()=>{
-        return[false]
-      })
+      setErrorCvc(false)
       return false
     }else{
-      setErrorCvc(()=>{
-        return[true]
-      })
+      setErrorCvc(true)
+      return true
     }
-    return true
   }
 
   function confirmCard(){
@@ -181,7 +165,7 @@ function App() {
     validarCvc(cvcRef.current.value);
     if(
       !validarName(cardNameRef.current.value) &&
-      !validarCardNumber(cardNumber) &&
+      !validarCardNumber(cardNumberRef.current.value) &&
       !validarDate(monthExpRef.current.value) &&
       !validarDate(yearExpRef.current.value) &&
       !validarCvc(cvcRef.current.value)
@@ -206,17 +190,13 @@ function App() {
     setCard((prevCards)=>{
       return[[...prevCards],{nameCard:name, numberCard:number, monthExp:month, yearExp:year, cvc:cvc}]
     })
-    setCardAdded(()=>{
-      return[true]
-    })
+    setCardAdded(true)
     updateForm()
   }
 
   function addOther(){
     resetCard();
-    setCardAdded(()=>{
-      return[false]
-    })
+    setCardAdded(false)
     updateForm()
   }
   function resetCard(){
@@ -238,7 +218,7 @@ function App() {
   };
 
   function updateForm() {
-    if (cardAdded[0]){
+    if (cardAdded){
       form = 
       <div className="ConfirmedCard">
         <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="40" cy="40" r="40" fill="url(#a)"/><path d="M28 39.92 36.08 48l16-16" stroke="#fff" stroke-width="3"/><defs><linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507" gradientUnits="userSpaceOnUse"><stop stop-color="#6348FE"/><stop offset="1" stop-color="#610595"/></linearGradient></defs></svg>
@@ -253,11 +233,11 @@ function App() {
       <form className="CardForm" onSubmit={validarSubmit}>
             <label htmlFor='cardholderName'>CARDHOLDER NAME
               <input ref={cardNameRef} type="text" placeholder='e.g. Jane Appleseed' name="cardholderName"  /* required='required' */ onChange={updateCardName}/>
-              {errorName[0] && <small>Can't be blank and must contain at least 2 letters</small>}
+              {errorName && <small>Can't be blank and must contain at least 2 letters</small>}
             </label>
             <label htmlFor='cardNumber'>CARD NUMBER
               <input ref={cardNumberRef} type="number" max="9999999999999999" placeholder='e.g. 1234 5678 9123 0000' name="cardNumber" id="cardNumber" /* required='required' */ onChange={updateCardNumber}/>
-              {errorCardNumber[0] && <small>Wrong format, numbers only or must contain at least 16 numbers.</small>}
+              {errorCardNumber && <small>Wrong format, numbers only or must contain at least 16 numbers.</small>}
             </label>
             <div className="GoupForm">
                 <label htmlFor='month'>EXP. DATE (MM/YY)
@@ -265,13 +245,13 @@ function App() {
                         <input ref={monthExpRef} min="1" max="12" type="number" placeholder='MM' name="month" id='month' /* required='required' */ onChange={updateMonthExp}/>
                         <input ref={yearExpRef} min="23" max="99" type="number" placeholder='YY' name="year" /* required='required' */ onChange={updateYearExp}/>
                     </div>
-                    {errorDate[0] && <small>Can't be blank and only numbers</small>}
+                    {errorDate && <small>Can't be blank and only numbers</small>}
                 </label>
                 <label htmlFor='cvc'>CVC
                     <div>
                         <input ref={cvcRef} max="999" type="number" placeholder='e.g. 123' name="cvc" id="cvc" /* required='required' */ onChange={updateCVC}/>
                     </div>
-                    {errorCvc[0] && <small>Can't be blank and only numbers</small>}
+                    {errorCvc && <small>Can't be blank and only numbers</small>}
                 </label>
             </div>
             <button className="ButtonConfirm" type="submit"> Confirm </button>
